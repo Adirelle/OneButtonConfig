77cd204ba9b5cc686182fda16a768e2e27d80943
Adirelle <adirelle@tagada-team.net>
2009-06-08 12:43:43 +0200

    Reworked built-in handler so they do not override legit handlers. Added built-in handlers for Grid, BigWigs, Baggins, Auracle, Gladius and Omen.

--- Modules/Modules.lua
+++ Modules/Modules.lua
@@ -1,26 +1,54 @@
--- Most handlers there are dirty and hackish and should better be implemented in the addon themselves
+--[[
+Most handlers there are dirty and hackish and should better be implemented in 
+the addon themselves.
+
+They all register a CONFIGMODE_CALLBACKS with the same as the addon except 
+when such callback already exists. So it will NOT override addon-defined 
+callback.
+
+If the addon is not yet loaded but could be loaded later, a listener 
+is set up to wait until the addon is loaded.
+]]
+
+--@debug@
+local debug = print
+--@end-debug@
+--[=[@non-debug@
+local debug = function() end
+--@end-non-debug@]=]
 
 local loadableModules
-local function RegisterModule(name, callback)
-	local safe_callback = function(...)
-		local res, msg = pcall(callback, ...)
+local function RegisterModule(names, callback, key)
+	if type(names) ~= "table" then
+		names = { names }
+	end
+	key = key or names[1]
+	local safe_callback = function()
+		if CONFIGMODE_CALLBACKS[key] then return debug("CONFIGMODE_CALLBACKS["..key.."] already exists") end
+		debug("Registering homemade",key,"handler")
+		local res, msg = pcall(callback)
 		if not res then
 			geterrorhandler()(msg)
 		end
 	end
-	if IsAddOnLoaded(name) then
-		safe_callback()
-	else
+	for _,name in pairs(names) do
+		if IsAddOnLoaded(name) then
+			debug(name, "already loaded, registering now")
+			return safe_callback()
+		end
+	end
+	for _,name in pairs(names) do
 		local realName, _, _, enabled, loadable = GetAddOnInfo(name)
 		if realName and enabled and loadable then
-			if not loadableModules then loadableModules = {} end			
-			loadableModules[name] = safe_callback
+			debug("Postpone", name, "registering")
+			if not loadableModules then loadableModules = {} end
+			loadableModules[name:lower()] = safe_callback
 		end
 	end
 end
 
 -- ag_UnitFrames
-RegisterModule('ag_unitframes', function()
+RegisterModule('ag_UnitFrames', function()
 	local aUF = LibStub("AceAddon-3.0"):GetAddon("ag_UnitFrames")
 	local backup = aUF.db.profile.Locked
 	function CONFIGMODE_CALLBACKS.ag_UnitFrames(action, mode)
@@ -45,7 +73,7 @@ RegisterModule('ag_unitframes', function()
 end)
 
 -- Dominos
-RegisterModule('dominos', function()
+RegisterModule('Dominos', function()
 	local Dominos = LibStub("AceAddon-3.0"):GetAddon("Dominos")
 	local backup = Dominos:Locked()
 	function CONFIGMODE_CALLBACKS.Dominos(action)
@@ -59,7 +87,7 @@ RegisterModule('dominos', function()
 end)
 
 -- SexyMap
-RegisterModule('sexymap', function()
+RegisterModule('SexyMap', function()
 	local SexyMap = LibStub("AceAddon-3.0"):GetAddon("SexyMap")
 	local General = SexyMap:GetModule('General')
 	function CONFIGMODE_CALLBACKS.SexyMap(action)
@@ -72,7 +100,7 @@ RegisterModule('sexymap', function()
 end)
 
 -- Quartz
-RegisterModule('quartz', function()
+RegisterModule('Quartz', function()
 	local Quartz = _G.Quartz
 	local qmodules = {
 		Player = 'lock',
@@ -103,18 +131,153 @@ RegisterModule('quartz', function()
 end)
 
 -- Cork
-RegisterModule('cork', function()
+RegisterModule('Cork', function()
 	local Cork = _G.Cork
-	local backup = Cork.anchor:IsShown()
+	local toggled = Cork.anchor:IsShown()
 	function CONFIGMODE_CALLBACKS.Cork(action)
 		if action == 'ON' then
-			backup = Cork.anchor:IsShown()
+			toggled = Cork.anchor:IsShown()
 			Cork.anchor:Show()
 		elseif action =='OFF' then
-			if not backup then
+			if not toggled then
 				Cork.anchor:Hide()
 			end
 		end
+	end
+end)
+
+-- Grid
+RegisterModule('Grid', function()
+	local toggled
+	function CONFIGMODE_CALLBACKS.Grid(action)
+		if action == 'ON' then	
+			toggled = GridLayout.options.args.lock.get()
+			GridLayout.options.args.lock.set(false)
+		elseif action == 'OFF' then
+			GridLayout.options.args.lock.set(toggled)
+		end
+	end
+end)
+
+-- BigWigs
+RegisterModule("BigWigs", function()
+	local function getModule(name)
+		return BigWigs:HasModule(name) and BigWigs:GetModule(name)
+	end
+	local toggledMessages
+	function CONFIGMODE_CALLBACKS.BigWigs(action)
+		if action ~= 'ON' and action ~= 'OFF' then return end
+		local show = (action == "ON")
+		local plugin = getModule('Messages')
+		if plugin then 
+			if show then
+				toggledMessages = plugin.consoleOptions.set("anchor")
+				plugin.consoleOptions.set("anchor", true)				
+			else
+				plugin.consoleOptions.set("anchor", toggledMessages)				
+			end
+		end
+		plugin = getModule('Proximity')
+		if plugin then
+			if show then
+				plugin:TestProximity()
+			else
+				plugin:CloseProximity()
+			end
+		end
+		plugin = getModule("Bars 2")
+		if plugin then
+			-- There is only a toggle, not lock/unlock
+			plugin:ShowAnchors()
+		end
+	end
+end)
+
+-- Baggins
+RegisterModule("Baggins", function()
+	local toggledPlacementFrame
+	local toggledLock
+	function CONFIGMODE_CALLBACKS.Baggins(action)
+		if Baggins.db.profile.layout == "auto" then
+			if action == "ON" then
+				toggledPlacementFrame = BagginsBagPlacement and BagginsBagPlacement:IsShown()
+				Baggins:ShowPlacementFrame() 
+			elseif action == "OFF" and BagginsBagPlacement then
+				if not toggledPlacementFrame then
+					BagginsBagPlacement:Hide()
+				end
+			end
+		else
+			if action == "ON" then
+				toggledLock = Baggins.db.profile.lock
+				Baggins.db.profile.lock = false
+			elseif action == "OFF" then
+				Baggins.db.profile.lock = toggledLock
+			end
+		end		
+	end
+end)
+
+-- Auracle
+RegisterModule('Auracle', function()
+	function CONFIGMODE_CALLBACKS.Auracle(action)
+		if action == 'ON' then
+			Auracle:UnlockWindows()
+		elseif action == 'OFF' then
+			Auracle:LockWindows()
+		end
+	end
+end)
+
+-- Gladius
+RegisterModule('Gladius', function()
+	local savedLocked
+	function CONFIGMODE_CALLBACKS.Gladius(action, mode)
+		if action == 'ON' then
+			savedLocked = Gladius.db.profile.locked
+			Gladius.db.profile.locked = false
+			
+			local bracket = tonumber(mode:sub(1,1))
+			if bracket ~= Gladius.currentBracket then
+				Gladius:ToggleFrame(bracket)
+			else
+				Gladius:UpdateFrame()
+			end
+		elseif action == 'OFF' then
+			Gladius.db.profile.locked = savedLocked
+			Gladius:HideFrame()
+			
+		elseif action == "GETMODES" then
+			return "5vs5", "3vs3", "2vs2"
+		end
+	end
+end)
+
+-- Omen
+RegisterModule('Omen', function()
+	local lockedInfo = {"Locked"}
+	local opts
+	local savedTestMode
+	local savedLocked
+	function CONFIGMODE_CALLBACKS.Omen(action)
+		if not opts then
+			if not Omen.Options then
+				Omen.GenerateOptionsInternal()
+			end
+			opts = Omen.Options.args.General.args
+		end
+		if action == 'ON' then
+			savedTestMode = opts.TestMode.get()
+			savedLocked = Omen.Options.get(lockedInfo)
+			opts.TestMode.get(nil, true)
+			opts.Locked.set(lockedInfo, false)
+			Omen:Toggle(true)
+		elseif action == 'OFF' then
+			opts.TestMode.set(nil, savedTestMode)
+			opts.Locked.set(lockedInfo, savedLocked)
+			Omen:Toggle(false)
+		end
+		
 	end
 end)
 
@@ -124,10 +287,14 @@ if loadableModules then
 	frame:RegisterEvent('ADDON_LOADED')
 	frame:SetScript('OnEvent', function(self, event, addon)
 		addon = addon:lower()
-		if loadableModules[addon] then
-			loadableModules[addon]()
-			loadableModules[addon] = nil
+		local callback = loadableModules[addon]
+		if callback then
+			for name,func in pairs(loadableModules) do
+				if func == callback then
+					loadableModules[name] = nil
+				end
+			end
+			callback()
 		end
-	end)	
+	end)
 end
-
